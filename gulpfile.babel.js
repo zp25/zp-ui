aa4a183585b7e7dd0098f1201b72e95d502c4330
zp25 <zebrap25@gmail.com>
2017-07-04 15:02:04 +0800

    build: 整理开发工具

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,11 +2,22 @@ import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 
 const $ = gulpLoadPlugins();
+const PATHS = {
+  scripts: [
+    'src/**/*.js',
+    'legacy/**/*.js',
+  ],
+};
 
-// Lint JavaScript
-const lint = () => gulp.src('src/**/*.js')
+const lint = () => gulp.src(PATHS.scripts)
   .pipe($.eslint())
   .pipe($.eslint.format());
 
+const watch = () => {
+  gulp.watch(PATHS.scripts, lint);
+};
+
 // Tasks
-gulp.task('default', lint);
+gulp.task(lint);
+
+gulp.task('default', watch);
