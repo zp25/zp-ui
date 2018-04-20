e72d4c09376083cc1325b8c4e9d22e38ecef8925
zp25 <zebrap25@gmail.com>
2018-04-20 16:46:14 +0800

    test: 更新mask为modal

--- test/modal.js
+++ test/modal.js
@@ -1,7 +1,7 @@
 import chai from 'chai';
 import sinon from 'sinon';
 import { JSDOM } from 'jsdom';
-import Mask from '../src/mask';
+import Modal from '../src/modal';
 import Util from '../src/util';
 
 chai.should();
@@ -20,55 +20,51 @@ const domStr = `
 <!DOCTYPE html>
 <html>
 <body>
-  <div class="mask" data-group="main">
-    <div class="mask__panel mask__panel--loading panel panel--black-reverse">
-      <div class="panel__body">
-        ${loading()}
-      </div>
+  <div class="modal" data-group="main">
+    <div class="modal__dialog modal__dialog--loading">
+      ${loading()}
     </div>
-    <div class="mask__panel mask__panel--message panel panel--black-reverse">
-      <div class="panel__body">
-        <p class="message"></p>
-      </div>
+    <div class="modal__dialog modal__dialog--message">
+      <p class="message"></p>
     </div>
   </div>
 </body>
 </html>
 `;
 
-describe('Mask', () => {
+describe('Modal', () => {
   describe('Basic', () => {
-    let mask = null;
+    let modal = null;
 
     before(() => {
       const window = new JSDOM(domStr).window;
       global.document = window.document;
 
-      mask = new Mask('main');
+      modal = new Modal('main');
     });
 
     it('Extends: Util', () => {
-      mask.should.be.an.instanceof(Util);
+      modal.should.be.an.instanceof(Util);
     });
 
     /**
      * Properties
      */
-    it('Prop: mask, 容器实例', () => {
-      const target = document.querySelector('.mask');
+    it('Prop: modal, 容器实例', () => {
+      const target = document.querySelector('.modal');
 
-      mask.mask.should.be.eql(target);
+      modal.modal.should.be.eql(target);
     });
   });
 
   describe('Methods', () => {
-    let mask = null;
-    // panels
+    let modal = null;
+    // dialog
     let loading = null;
     let prompt = null;
 
-    const maskActiveName = 'mask--active';
-    const panelActiveName = 'mask__panel--active';
+    const modalActiveName = 'modal--active';
+    const dialogActiveName = 'modal__dialog--active';
 
     const msgList = [
       ['custome message', 'custome message'],
@@ -85,79 +81,79 @@ describe('Mask', () => {
       const window = new JSDOM(domStr).window;
       global.document = window.document;
 
-      mask = new Mask('main');
+      modal = new Modal('main');
 
-      loading = document.querySelector('.mask__panel--loading');
-      prompt = document.querySelector('.mask__panel--message');
+      loading = document.querySelector('.modal__dialog--loading');
+      prompt = document.querySelector('.modal__dialog--message');
     });
 
-    it('prompt, 显示mask并正确切换panel，可自定义message', () => {
+    it('prompt, 显示modal并正确切换dialog，可自定义message', () => {
       const [raw, encoded] = msgList[0];
       const state = {
         'loading': false,
         'message': true,
       };
 
-      mask.prompt('message', raw);
+      modal.prompt('message', raw);
 
       const result = {
-        'loading': loading.classList.contains(panelActiveName),
-        'message': prompt.classList.contains(panelActiveName),
+        'loading': loading.classList.contains(dialogActiveName),
+        'message': prompt.classList.contains(dialogActiveName),
       };
 
-      // mask状态
-      mask.mask.classList.contains(maskActiveName).should.be.true;
-      // panel状态
+      // modal状态
+      modal.modal.classList.contains(modalActiveName).should.be.true;
+      // dialog状态
       result.should.be.eql(state);
       // 自定义message
       prompt.querySelector('.message').innerHTML.should.equal(encoded);
     });
 
-    it('prompt, 无匹配panel将抛出Error', () => {
-      const func = () => { mask.prompt('nopanel'); };
+    it('prompt, 无匹配dialog将抛出Error', () => {
+      const func = () => { modal.prompt('nopanel'); };
 
       func.should.throw(Error);
     });
 
     it('prompt, 能处理多种数据类型message', () => {
       msgList.forEach(([raw, encoded]) => {
-        mask.prompt('message', raw);
+        modal.prompt('message', raw);
         prompt.querySelector('.message').innerHTML.should.equal(encoded);
       });
     });
 
     it('loading, prompt语法糖', () => {
       const spy = sinon.spy();
-      mask.prompt = spy;
+      modal.prompt = spy;
 
-      mask.loading();
+      modal.loading();
 
       spy.calledWithExactly('loading').should.be.true;
     });
 
-    it('show, 显示mask，不显示任何panel', () => {
+    it('open, 显示modal，不显示任何dialog', () => {
       const state = {
         'loading': false,
         'message': false,
       };
 
-      mask.show();
+      modal.open();
 
       const result = {
-        'loading': loading.classList.contains(panelActiveName),
-        'message': prompt.classList.contains(panelActiveName),
+        'loading': loading.classList.contains(dialogActiveName),
+        'message': prompt.classList.contains(dialogActiveName),
       };
 
-      // mask状态
-      mask.mask.classList.contains(maskActiveName).should.be.true;
-      // panel状态
+      // modal状态
+      modal.modal.classList.contains(modalActiveName).should.be.true;
+      // dialog状态
       result.should.be.eql(state);
     });
 
-    it('hide, 隐藏mask', () => {
-      mask.hide();
+    it('close, 隐藏modal', () => {
+      modal.close();
 
-      mask.mask.classList.contains(maskActiveName).should.be.false;
+      modal.modal.classList.contains(modalActiveName).should.be.false;
     });
   });
 });
